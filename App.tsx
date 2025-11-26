0a75d3dcadd9cdd95d930049eb85a24875433c0c
snattar <snattar1968@gmail.com>
2025-11-26 20:41:39 +0530

    feat: Update dev dependencies and React import
    Updates Node.js types in dev dependencies and refactors React import to be more specific.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Header } from './components/Header';
 import { ControlPanel } from './components/ControlPanel';
 import { AITutor } from './components/AITutor';
